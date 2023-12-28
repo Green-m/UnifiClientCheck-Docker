1e3a86bc0691e83fb2b402b395348be95b70bcea
Green-m <root@green-m.me>
2023-12-28 17:20:34 +0800

    Feat: add guestlist and save known clients to disk.

--- src/UniFiClientAlerts.php
+++ src/UniFiClientAlerts.php
@@ -10,6 +10,35 @@ $knownMacs = explode(',', getenv('KNOWN_MACS')); // MAC addresses are comma-sepa
 $checkInterval = getenv('CHECK_INTERVAL') ?: 60; // Time in seconds
 $telegramBotToken = getenv('TELEGRAM_BOT_TOKEN');
 $telegramChatId = getenv('TELEGRAM_CHAT_ID');
+$guestSubnet = getenv('GUEST_SUBNET'); // 10.1.0.0
+$guestSubnetMask = getenv('GUEST_SUBNET_MASK'); // 255.255.255.0
+
+
+function loadKnownClients() {
+    $filePath = __DIR__ . '/known_clients.json';
+    if (file_exists($filePath)) {
+        $json = file_get_contents($filePath);
+        $data = json_decode($json, true);
+        if (is_array($data)) {
+            return $data;
+        }
+    }
+    return [];
+}
+
+function saveKnownClients($clients) {
+    $filePath = __DIR__ . '/known_clients.json';
+    $json = json_encode($clients);
+    file_put_contents($filePath, $json);
+}
+
+function isInGuestlistedSubnet($ip, $subnet, $mask) {
+    $ip_long = ip2long($ip);
+    $subnet_long = ip2long($subnet);
+    $mask_long = ip2long($mask);
+
+    return ($ip_long & $mask_long) == ($subnet_long & $mask_long);
+}
 
 function createUnifiClient() {
     global $controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion;
@@ -24,6 +53,8 @@ $telegramClient = new GuzzleClient([
     'base_uri' => 'https://api.telegram.org'
 ]);
 
+$knownClients = loadKnownClients();
+
 while (true) {
     $clients = $unifiClient->list_clients();
     
@@ -37,9 +68,10 @@ while (true) {
         $newDeviceFound = false;
 
         foreach ($clients as $client) {
-            if (!in_array($client->mac, $knownMacs)) {
+            if (!array_key_exists($client->mac, $knownClients) && 
+                !isInGuestlistedSubnet($client->ip, $guestSubnet, $guestSubnetMask))  {
                 $newDeviceFound = true;
-                echo "New device found. Sending a notification.\n";
+
                 $message = "New device seen on network\n";
                 $message .= "Device Name: " . ($client->name ?? 'Unknown') . "\n";
                 $message .= "IP Address: " . $client->ip . "\n";
@@ -48,6 +80,10 @@ while (true) {
                 $message .= "Connection Type: " . ($client->is_wired ? "Wired" : "Wireless") . "\n";
                 $message .= "Network: " . ($client->network ?? 'N/A');
 
+                $deviceName = $client->name ?? 'Unknown'; 
+                $ipAddress = $client->ip ?? 'N/A';
+
+                echo "New device found: Name - {$deviceName}, IP - {$ipAddress}. Sending a notification.\n";
                 $telegramClient->post("/bot{$telegramBotToken}/sendMessage", [
                     'json' => [
                         'chat_id' => $telegramChatId,
@@ -55,17 +91,19 @@ while (true) {
                     ]
                 ]);
 
-                $knownMacs[] = $client->mac;
+                $knownClients[$client->mac] = (array)$client;
             }
         }
 
         if (!$newDeviceFound) {
-            echo "No new devices found on the network.\n";
+            #echo "No new devices found on the network.\n";
+            sleep(1);
         }
     } else {
         echo "No clients currently connected to the network.\n";
     }
 
+    saveKnownClients($knownClients); 
     echo "Checking in {$checkInterval} seconds...\n";
     sleep($checkInterval);
 }
